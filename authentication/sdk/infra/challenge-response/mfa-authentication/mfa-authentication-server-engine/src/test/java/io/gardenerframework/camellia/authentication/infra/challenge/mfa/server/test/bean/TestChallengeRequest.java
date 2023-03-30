5981ea1cc18ce9107dabfa7870083f5a443e9b58
zhanghan30 <096%*g17qcHRJuHJx5b0000000>
2023-03-30 17:40:22 +0800

    feat(mfa-authentication-server): 完成mfa认证服务器客户端的开发

---
+++ authentication/sdk/infra/challenge-response/mfa-authentication/mfa-authentication-server-engine/src/test/java/io/gardenerframework/camellia/authentication/infra/challenge/mfa/server/test/bean/TestChallengeRequest.java
@@ -0,0 +1,10 @@
+package io.gardenerframework.camellia.authentication.infra.challenge.mfa.server.test.bean;
+
+import io.gardenerframework.camellia.authentication.infra.challenge.core.schema.ChallengeRequest;
+
+/**
+ * @author zhanghan30
+ * @date 2023/3/29 18:12
+ */
+public class TestChallengeRequest implements ChallengeRequest {
+}
