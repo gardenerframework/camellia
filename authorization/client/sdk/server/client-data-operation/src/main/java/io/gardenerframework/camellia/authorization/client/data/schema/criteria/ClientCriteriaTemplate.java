335a4299dfdc6a0d3cbec68ae27b528e744b62cf
张涵 <zhanghan30@jd.co>
2023-10-27 00:06:18 +0800

    chore(client): 客户端搜索条件增加@Nullable注解

--- authorization/client/sdk/server/client-data-operation/src/main/java/io/gardenerframework/camellia/authorization/client/data/schema/criteria/ClientCriteriaTemplate.java
+++ authorization/client/sdk/server/client-data-operation/src/main/java/io/gardenerframework/camellia/authorization/client/data/schema/criteria/ClientCriteriaTemplate.java
@@ -5,6 +5,7 @@ import io.gardenerframework.fragrans.data.trait.generic.GenericTraits;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+import org.springframework.lang.Nullable;
 
 /**
  * @author chris
@@ -19,5 +20,6 @@ public class ClientCriteriaTemplate implements
      * 应用的名称，判等而不是包含
      */
     @Equals
+    @Nullable
     private String name;
 }
